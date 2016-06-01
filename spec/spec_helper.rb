8cae05220ef4a6d5c5d36cf40c9f4728ec0c95f5
Michael Stock <mikeastock@gmail.com>
2016-05-31 22:40:49 -0700

    Set RACK_ENV in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require "sidecar"
 
+ENV["RACK_ENV"] = "test"
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
